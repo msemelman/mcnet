2304d56237f67fcc4a605c6c81556382d37927df
Mariano Semelman <marianosemelman@gmail.com>
2015-05-10 23:04:56 -0300

    chau mapcrafter por un rato

--- minecraft.sh
+++ minecraft.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-cd /home/ubuntu/minecraft-server/;
-./mapcrafter.sh
-java -Xmx800M -Xms800M -jar minecraft_server.1.8.4.jar nogui >> /home/ubuntu/minecraft-server/server.log
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
+echo $DIR;
+cd $DIR;
+#./mapcrafter.sh $DIR/render.conf
+screen -S 'minecraft' -d -m  java -Xmx800M -Xms800M -jar $DIR/minecraft_server.1.8.4.jar nogui >> $DIR/server.log
