7d4eb5223aa63475c6452c967198bb00c432b38c
Mariano Semelman <marianosemelman@gmail.com>
2015-05-12 23:33:39 -0300

    ahi va archivo

--- minecraft.sh
+++ minecraft.sh
@@ -5,4 +5,5 @@ cd $DIR;
 #./mapcrafter.sh $DIR/render.conf
 
 screen -L -S 'minecraft' -d -m  
-screen -S minecraft -X stuff "java -Xmx800M -XX:+AggressiveOpts -Xms800M -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalPacing -jar $DIR/minecraft_server.1.8.4.jar nogui 2>&1 > server.log"$"\n"
+sleep 0.5
+screen -S 'minecraft' -X stuff "java -Xmx800M -XX:+AggressiveOpts -Xms800M -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalPacing -jar $DIR/minecraft_server.1.8.4.jar nogui 2>&1 > server.log"$"\n"
