d262551eb9feafffbb85724ea48b4a1a30b4eaf9
Mariano Semelman <marianosemelman@gmail.com>
2015-05-13 00:15:23 -0300

    avance

--- minecraft.sh
+++ minecraft.sh
@@ -5,5 +5,5 @@ cd $DIR;
 #./mapcrafter.sh $DIR/render.conf
 
 screen -L -S 'minecraft' -d -m  
-sleep 0.5
+sleep 1
 screen -S 'minecraft' -X stuff "java -Xmx800M -XX:+AggressiveOpts -Xms800M -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalPacing -jar $DIR/minecraft_server.1.8.4.jar nogui 2>&1 > server.log"$"\n"
---
+++ watch-server.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
+cd $DIR;
+
+HITS=$DIR/hits.log
+JUGADORES=$(./mccommand /list | grep -ho '[0-9]*/20'|grep -Pho '[0-9]*(?=/20)');
+if [ "$JUGADORES" = "" ]; then
+    echo "Server down";
+    exit;
+fi
+echo "Hay "$JUGADORES" jugadores"
+if [ $JUGADORES = 0 ]; then
+    echo 'strike' >> $HITS;
+    echo $(wc -l $HITS | awk '{print $1}');
+    if [ $(wc -l $HITS | awk '{print $1}') -gt 30 ]; then
+        echo 'OUUUUT';
+        ./mccommand /stop;
+        sleep 4;
+        ./mccommand exit;
+    fi
+else 
+    echo 'Salvado por la campana';
+    rm -f $HITS
+fi;
+
